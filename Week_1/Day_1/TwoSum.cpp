2eeeaccb022d5f7f4864a4204711d6e8ba3ec993
Sagar Tiwari <24112fw011@glbitm.ac.in>
2026-06-01 21:31:35 +0530

    Add files via upload

--- Week_1/Day_1/TwoSum.cpp
+++ Week_1/Day_1/TwoSum.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        
+         unordered_map<int,int>m;
+         vector<int>ans;
+         for(int i=0;i<n;i++)
+         {
+            m[nums[i]]=i;
+         }
 
+         for(int j=0;j<n;j++)
+         {
+            int key=target-nums[j];
+            if(m.count(key)==1 && m[key]!=j)
+            {
+                return {m[key],j};
+            }
+           
+            
+         } 
+         return {0,0};
+
+
+            
+
+    }
+};
