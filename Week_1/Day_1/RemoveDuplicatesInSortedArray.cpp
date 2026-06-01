8c7fe0ca02343c579d27285aee22dcef3473f831
Sagar Tiwari <24112fw011@glbitm.ac.in>
2026-06-01 21:51:39 +0530

    Add files via upload

---
+++ Week_1/Day_1/RemoveDuplicatesInSortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int n=nums.size();
+        int i=0,j=1;
+        while(i<n-1)
+        {
+        
+            if(nums[i]!=nums[i+1])
+            {
+                nums[j]=nums[i+1];
+                j++;
+                
+            }
+            i++;
+        }
+        return j;
+    }
+};
