37505c8fb5bb7f80cee401e83c81608012b1ce41
Sagar Tiwari <24112fw011@glbitm.ac.in>
2026-06-02 21:57:35 +0530

    Implement containsDuplicate function in C++

---
+++ Week_1/Day_2/ContainsDuplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto j=m.begin();j!=m.end();j++)
+        {
+            if(j->second>=2)
+            return 1;
+        }
+        return 0;
+        
+        
+    }
+};
